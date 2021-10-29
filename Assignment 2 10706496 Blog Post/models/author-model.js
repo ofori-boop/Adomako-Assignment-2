86588635a0d7a09df5f089e56929e97fc3063da0
ofori-boop <84097239+ofori-boop@users.noreply.github.com>
2021-10-29 17:54:08 +0000

    Add files via upload

---
+++ Assignment 2 10706496 Blog Post/models/author-model.js
@@ -0,0 +1,21 @@
+// 10706496 Adomako Ofori Emmanuel 
+import mongoose from "mongoose";
+import { postSchema } from "./post-model";
+const { Schema, model } = mongoose;
+
+const authorSchema = new Schema({
+  //name of author
+  name: {
+    type: String,
+    required: true,
+  },
+
+  //details of the post
+  posts: {
+    type: [postSchema],
+  },
+});
+
+const authorModel = model("author", authorSchema);
+
+export default authorModel;
